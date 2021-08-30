1aaa014de325d06a2f6284ae4b0a9fb23b947ef0
Lena Ebner <lena.ebner@mway.io>
2021-08-30 15:24:12 +0200

    add sqflite database

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import '../models/place.dart';
 import 'dart:io';
+import '../helpers/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
+  static const tableName = "user_places";
   List<Place> _items = [];
 
   List<Place> get items {
@@ -17,6 +19,24 @@ class GreatPlaces with ChangeNotifier {
       location: null,
     );
     _items.add(newPlace);
+    notifyListeners();
+
+    DBHelper.insert(tableName, {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final data = await DBHelper.getData(tableName);
+    _items = data
+        .map((item) => Place(
+            id: item['id'] as String,
+            title: item['title'] as String,
+            location: null,
+            image: File(item['image'] as String)))
+        .toList();
     notifyListeners();
   }
 }
--- lib/screens/places_screen.dart
+++ lib/screens/places_screen.dart
@@ -18,36 +18,47 @@ class PlacesScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Got not places yet, start adding one."),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
-                },
-                icon: Icon(Icons.add),
-                label: Text("Add a new place"),
-              ),
-            ],
-          ),
-        ),
-        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
-            ? ch!
-            : ListView.builder(
-                itemBuilder: (ctx, i) => ListTile(
-                  title: Text(greatPlaces.items[i].title),
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(greatPlaces.items[i].image),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Consumer<GreatPlaces>(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Got not places yet, start adding one."),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.of(context)
+                              .pushNamed(AddPlaceScreen.routeName);
+                        },
+                        icon: Icon(Icons.add),
+                        label: Text("Add a new place"),
+                      ),
+                    ],
                   ),
-                  onTap: () {},
                 ),
-                itemCount: greatPlaces.items.length,
+                builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+                    ? ch!
+                    : ListView.builder(
+                        itemBuilder: (ctx, i) => ListTile(
+                          title: Text(greatPlaces.items[i].title),
+                          leading: CircleAvatar(
+                            backgroundImage:
+                                FileImage(greatPlaces.items[i].image),
+                          ),
+                          onTap: () {},
+                        ),
+                        itemCount: greatPlaces.items.length,
+                      ),
               ),
       ),
     );
